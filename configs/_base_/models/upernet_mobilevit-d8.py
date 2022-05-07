4c32d6801403c71683b9d13a3e3ccd013e656a8b
Harrison Termotto <htermotto@gmail.com>
2022-05-07 16:34:15 -0400

    Deeplab config

--- configs/_base_/models/upernet_mobilevit-d8.py
+++ configs/_base_/models/upernet_mobilevit-d8.py
@@ -4,23 +4,23 @@ model = dict(
     type='EncoderDecoder',
     backbone=dict(
         type='MobileViT',
-        image_size=((512,1024)),
+        image_size=((256,256)),
         dims=[64, 80, 96],
         channels=[16, 16, 24, 24, 48, 48, 64, 64, 80, 80, 320],
-        num_classes=19,
-        pretrained_path='/content/mmsegmentation/pretrained_weights/deeplabv3_mobilevit_xxs.pt',
+        num_classes=1000,
+        pretrained_path='pretrained_weights/deeplabv3_mobilevit_xxs.pt',
     ),
-    # neck=dict(
-    #     type='MultiLevelNeck',
-    #     in_channels=[320, 320, 320, 320],
-    #     out_channels=320,
-    #     scales=[4, 2, 1, 0.5]),
+    neck=dict(
+        type='MultiLevelNeck',
+        in_channels=[320],
+        out_channels=768,
+        scales=[4, 2, 1, 0.5]),
     decode_head=dict(
-        type='ASPPHead',
-        in_channels=320,
-        in_index=3,
-        channels=256,
-        dilations=(6,12,18),
+        type='UPerHead',
+        in_channels=[768, 768, 768, 768],
+        in_index=[0, 1, 2, 3],
+        pool_scales=(1, 2, 3, 6),
+        channels=512,
         dropout_ratio=0.1,
         num_classes=19,
         norm_cfg=norm_cfg,
@@ -29,8 +29,8 @@ model = dict(
             type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
     auxiliary_head=dict(
         type='FCNHead',
-        in_channels=320,
-        in_index=2,
+        in_channels=768,
+        in_index=3,
         channels=256,
         num_convs=1,
         concat_input=False,
@@ -42,4 +42,4 @@ model = dict(
             type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)),
     # model training and testing settings
     train_cfg=dict(),
-    test_cfg=dict(mode='whole'))
+    test_cfg=dict(mode='whole'))  # yapf: disable
