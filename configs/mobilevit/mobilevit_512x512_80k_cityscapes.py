a98877a8a44792243cb6699e1cb798dffa98a075
Harrison Termotto <htermotto@gmail.com>
2022-05-07 17:35:01 -0400

    go back to upernet

--- configs/mobilevit/mobilevit_512x512_80k_cityscapes.py
+++ configs/mobilevit/mobilevit_512x512_80k_cityscapes.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/models/deeplabv3_mobilevit-d8.py', '../_base_/datasets/ade20k.py',
+    '../_base_/models/upernet_mobilevit-d8.py', '../_base_/datasets/cityscapes.py',
     '../_base_/default_runtime.py', '../_base_/schedules/schedule_80k.py'
 ]
 model = dict(
@@ -7,6 +7,6 @@ model = dict(
     backbone=dict(
         type='MobileViT',
     ),
-    decode_head=dict(num_classes=150),
-    auxiliary_head=dict(num_classes=150),
+    decode_head=dict(num_classes=19),
+    auxiliary_head=dict(num_classes=19),
 )
