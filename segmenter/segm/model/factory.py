d6365c74bb127ef181c95ad5b8163fa9288c91f0
Harrison Termotto <htermotto@gmail.com>
2022-05-08 13:08:31 -0400

    load mobilevit

--- segmenter/segm/model/factory.py
+++ segmenter/segm/model/factory.py
@@ -17,7 +17,7 @@ from segm.model.decoder import MaskTransformer
 from segm.model.segmenter import Segmenter
 import segm.utils.torch as ptu
 
-# from mmseg.models.backbones.mobilevit import MobileViT
+from mmseg.models.backbones.mobilevit import MobileViT
 
 
 @register_model
@@ -67,18 +67,19 @@ def create_vit(model_cfg):
         model_cfg["image_size"][1],
     )
 
-    model = VisionTransformer(**model_cfg)
-    if backbone == "vit_base_patch8_384":
-        path = os.path.expandvars("$TORCH_HOME/hub/checkpoints/vit_base_patch8_384.pth")
-        state_dict = torch.load(path, map_location="cpu")
-        filtered_dict = checkpoint_filter_fn(state_dict, model)
-        model.load_state_dict(filtered_dict, strict=True)
-    elif "deit" in backbone:
-        load_pretrained(model, default_cfg, filter_fn=checkpoint_filter_fn)
-    # elif backbone == "mobilevit":
-    #     model = MobileViT(**model_cfg)
+    if backbone == "mobilevit":
+        model = MobileViT(**model_cfg)
     else:
-        load_custom_pretrained(model, default_cfg)
+        model = VisionTransformer(**model_cfg)
+        if backbone == "vit_base_patch8_384":
+            path = os.path.expandvars("$TORCH_HOME/hub/checkpoints/vit_base_patch8_384.pth")
+            state_dict = torch.load(path, map_location="cpu")
+            filtered_dict = checkpoint_filter_fn(state_dict, model)
+            model.load_state_dict(filtered_dict, strict=True)
+        elif "deit" in backbone:
+            load_pretrained(model, default_cfg, filter_fn=checkpoint_filter_fn)
+        else:
+            load_custom_pretrained(model, default_cfg)
 
     return model
 
